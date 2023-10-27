d26ec41133050234aa9f3413b62c0eadcb5a14ae
kristiyan-kalinov <kristiyan.kalinov9@gmail.com>
2023-10-27 14:55:41 +0300

    fixed subscription tax

--- src/main/java/com/kodar/academy/Library/service/UserService.java
+++ src/main/java/com/kodar/academy/Library/service/UserService.java
@@ -158,7 +158,7 @@ public class UserService {
     private static BigDecimal amountToPay(Subscription subscription) {
         return subscription.getCost()
                 .divide(BigDecimal.valueOf(LocalDate.now().lengthOfMonth()), RoundingMode.UP)
-                .multiply(BigDecimal.valueOf(LocalDate.now().lengthOfMonth() - LocalDate.now().getDayOfMonth()));
+                .multiply(BigDecimal.valueOf(LocalDate.now().lengthOfMonth() - LocalDate.now().getDayOfMonth() + 1));
     }
 
     @Transactional
