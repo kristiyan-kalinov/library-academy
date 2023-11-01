4cae8cf73105a23f0ec8157e540aed7b16661ea5
kristiyan-kalinov <kristiyan.kalinov9@gmail.com>
2023-11-01 09:23:32 +0200

    xml mapping annotation

--- src/main/java/com/kodar/academy/Library/model/dto/book/BookCreateDTO.java
+++ src/main/java/com/kodar/academy/Library/model/dto/book/BookCreateDTO.java
@@ -1,5 +1,7 @@
 package com.kodar.academy.Library.model.dto.book;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.kodar.academy.Library.model.constants.Constants;
 import com.kodar.academy.Library.model.dto.author.AuthorDTO;
 import com.kodar.academy.Library.model.dto.genre.GenreDTO;
@@ -11,22 +13,30 @@ import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
+@JacksonXmlRootElement(localName = "book")
 public class BookCreateDTO {
 
+    @JacksonXmlProperty(localName = "isbn")
     @NotBlank(message = Constants.ISBN_REQUIRED)
     @UniqueIsbn
 //    @Pattern(regexp = "^(?=(?:\\D*\\d){13}\\D*$)\\d{1,5}[-\\s]?\\d{1,7}[-\\s]?\\d{1,6}[-\\s]?\\d{1}$\n",
 //            message = "Invalid ISBN format")
     private String isbn;
+    @JacksonXmlProperty(localName = "title")
     @NotBlank(message = Constants.TITLE_REQUIRED)
     @Size(min = 1, max = 255, message = Constants.TITLE_LENGTH)
     private String title;
+    @JacksonXmlProperty(localName = "publisher")
     @NotBlank(message = Constants.PUBLISHER_REQUIRED)
     @Size(min = 2, max = 64, message = Constants.PUBLISHER_LENGTH)
     private String publisher;
+    @JacksonXmlProperty(localName = "year")
     private short year;
+    @JacksonXmlProperty(localName = "genres")
     private Set<@Valid GenreDTO> genres;
+    @JacksonXmlProperty(localName = "authors")
     private Set<@Valid AuthorDTO> authors;
+    @JacksonXmlProperty(localName = "totalQuantity")
     @Min(value = 0, message = Constants.TOTAL_QUANTITY_MIN_VALUE)
     private int totalQuantity;
 
